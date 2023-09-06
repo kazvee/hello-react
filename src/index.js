61dc4c762cb378c5aaae7cffe827028a4488dfd5
kazvee <kayvee123@gmail.com>
2023-09-06 16:09:58 -0600

    Refactor Application component input field to be controlled component

--- src/index.js
+++ src/index.js
@@ -3,25 +3,32 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 const Button = (props) => {
-  return <button onClick={props.reset} value={props.children}>Reset</button>;
+  return (
+    <button onClick={props.reset} value={props.children}>
+      Reset
+    </button>
+  );
 };
 
 const Application = (props) => {
-
-  const [name, setName] = useState("Sunshine")  
+  const [name, setName] = useState('');
 
   const reset = () => {
-    console.log("Reset! 🤠");
+    console.log('Reset! 🤠');
     // your code here
   };
 
   return (
     <main>
-      <input placeholder='Please type in your name'></input>
-      <Button reset={reset} name={setName}/>
+      <input
+        value={name}
+        onChange={(event) => setName(event.target.value)}
+        placeholder='Please type in your name'
+      ></input>
+      <Button reset={reset} name={setName} />
       <h1>Hello {name}! 🌞</h1>
     </main>
   );
 };
 
-ReactDOM.render(<Application />, document.getElementById("root"));
+ReactDOM.render(<Application />, document.getElementById('root'));
